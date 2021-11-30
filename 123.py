58336f3cb3b59824ff25fed971395cc280ab35fc
Hung Yu Lin <aa25622515@gmail.com>
2021-12-01 02:19:02 +0800

    add new file

---
+++ 123.py
@@ -0,0 +1,23 @@
+x = 5
+print(x)
+# 整數 integer int
+# 小數 float
+# 布林值 boolean bool(true,false)
+# 字串 string str('文字')
+
+a = 45
+b = 2.2
+c = True
+d = 'hello'
+e = "hello"
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+
+
+height = input('輸入身高')
+weight = input('輸入體重')
+print('身高:' + height + ',' + '體重:' + weight  )
